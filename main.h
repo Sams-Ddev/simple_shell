47f5bc4159072bba5f5f5641b8371170a459ba56
Sams-Ddev <sams.alxstudent@gmail.com>
2023-11-09 13:28:07 +0000

    started a main.h

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+
+
+#endif /* main.h */
